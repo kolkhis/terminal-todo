0cd68e0fca00b7b5f9bab31297b393a79d929692
Kolkhis <36500473+Kolkhis@users.noreply.github.com>
2025-01-16 20:07:08 -0500

    feat: Add checks for storage file and its directory, create them if they don't exist

--- internal/tasks/tasks.go
+++ internal/tasks/tasks.go
@@ -1,6 +1,7 @@
 package tasks
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -53,13 +54,22 @@ func NewTask(title, desc string, id int) Task {
 	return t
 }
 
+func (tl *TaskList) DeleteTask(t Task) {
+	for i := range len(tl.Tasks) {
+		if t.Id == tl.Tasks[i].Id {
+			// remove from list
+			// tl.Tasks[i]
+		}
+	}
+}
+
 func (tl *TaskList) AddTaskToList(t Task) {
 	t.Id = tl.nextTaskId
 	tl.Tasks = append(tl.Tasks, t)
 	tl.nextTaskId++
 }
 
-func (t *Task) formatOutput() string {
+func (t *Task) formatTaskOutput() string {
 	outputStr := fmt.Sprintf(`
     - Task
       %vTitle: %v
@@ -80,14 +90,12 @@ func (t *Task) formatOutput() string {
 	return outputStr
 }
 
-func (tl *TaskList) DeleteTask(t Task) {}
-
 // ViewTaskList Outputs the current task list to the terminal
 // TODO: Add method (or option) to show only incomplete tasks, and only complete tasks
 func (tl *TaskList) ViewTaskList() {
 	fmt.Println("\033[32m--- All tasks ---\033[0m")
 	for idx := range tl.Tasks {
-		output := tl.Tasks[idx].formatOutput()
+		output := tl.Tasks[idx].formatTaskOutput()
 		fmt.Println(output)
 	}
 }
@@ -96,7 +104,7 @@ func (tl *TaskList) ViewCompletedTasks() {
 	fmt.Println("--- Completed Tasks ---")
 	completedTasks := tl.GetCompletedTasks()
 	for idx := range completedTasks {
-		output := completedTasks[idx].formatOutput()
+		output := completedTasks[idx].formatTaskOutput()
 		fmt.Println(output)
 	}
 }
@@ -113,29 +121,73 @@ func (tl *TaskList) GetCompletedTasks() []*Task {
 	return ct
 }
 
-// TODO: Add UI for these.
-// MarkComplete() marks a task as completed, changing its Completed property to true
-func (t *Task) MarkComplete() { t.Completed = true }
+// Check the storage file path, create it if it doesn't exist.
+func checkStorageFileDir(basepath string) bool {
+	fmt.Printf("\033[31m --- Base path for storage file: %v ---\n\033[0m", basepath)
+	// Create file if it's not there
+	// Create directory if it's not there
+	fileInfo, err := os.Stat(basepath)
+	if err != nil {
+		fmt.Printf("Failed to stat directory at %v: %v\n", basepath, err)
+		fmt.Printf("Attempting to create directory: %v\n", basepath)
+		err = os.MkdirAll(basepath, os.FileMode(0755))
+		if err != nil {
+			fmt.Printf(Colors["red"]+"Error creating directory at %v: %v\n"+Colors[""], basepath, err)
+			return false
+		} else {
+			fmt.Printf("\033[32mSuccessfully created directory: %v\n", basepath)
+		}
+	} else if fileInfo.IsDir() {
+		return true
+	}
+	fmt.Printf("Couldn't determine the existence of the directory: %v\n", basepath)
+	return false
+}
 
-func (t *Task) ChangeDescription(newDesc string) { t.Description = newDesc }
-func (t *Task) ChangeTitle(newTitle string)      { t.Title = newTitle }
+func checkStorageFile(basepath string) bool {
+	storageFile := basepath + "tasklist.json"
+	_, err := os.Stat(storageFile)
+	if err != nil {
+		fmt.Printf(Colors["red"]+"Failed to stat file at %v: %v\n"+Colors[""], storageFile, err)
+		if errors.Is(err, os.ErrNotExist) {
+			fmt.Printf(Colors["red"]+"Error - File %v doesn't exist: %v\n"+Colors[""], storageFile, err)
+			fmt.Printf("Storage file does not exist at %s.\nCreating one...\n", storageFile)
+			return false
+		} else {
+			fmt.Printf("Ran into error while checking directory: %v\n", err)
+		}
+	}
+
+	_, err = os.Create(storageFile)
+
+	if err != nil {
+		fmt.Printf("Ran into error when creating file %v: %v\n", storageFile, err)
+		return false
+	} else {
+		return true
+	}
+}
 
 func (*TaskList) LoadTaskList() {
-	path := "/etc/terminal-todo/tasklist.json"
-	fileInfo, err := os.Stat(path)
-	if err != nil {
-		fmt.Printf("Storage file does not exist at %s. Creating one.\n", path)
-		os.Create(path)
-	} else {
-		fmt.Printf("fileInfo: %s\n", fileInfo)
-		fmt.Printf("fileinfo.IsDir: %v\n", fileInfo.IsDir())
+	basepath := os.Getenv(`HOME`) + "/.config/terminal-todo/" // Don't use tilde, no pathname expansion
+	if !checkStorageFileDir(basepath) {
+		log.Fatalf("Unable to create the storage file directory in %v\n", basepath)
 	}
-	fmt.Printf("Attempting to open file at; %v\n", path)
-	file, err := os.Open(path)
+	if !checkStorageFile(basepath) {
+		log.Fatalf("Unable to load storage file at %v\n", basepath)
+	}
+
+	storageFile := basepath + "tasklist.json"
+	log.Printf("Opening file at: %v\n", storageFile)
+
+	file, err := os.Open(storageFile)
 	if err != nil {
-		log.Printf("Couldn't open file /etc/terminal-todo/tasklist.json")
+		log.Fatalf("Couldn't open file storage file at %v\n", storageFile)
 	}
 	defer file.Close()
+	// TODO: Add logic to check if file is empty -
+	// TODO: If not empty, parse contents into a TaskList()
+
 }
 
 func (*TaskList) SaveTaskList() {}
@@ -152,3 +204,8 @@ func (tl *TaskList) ShowType() {
 }
 
 // git commit -m "go: Update notes on interfaces, move function notes to different file"
+// TODO: Add UI for these.
+// MarkComplete() marks a task as completed, changing its Completed property to true
+func (t *Task) MarkComplete()                    { t.Completed = true }
+func (t *Task) ChangeDescription(newDesc string) { t.Description = newDesc }
+func (t *Task) ChangeTitle(newTitle string)      { t.Title = newTitle }
